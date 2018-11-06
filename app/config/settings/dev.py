bbd266cb380d2c72472d7745931448b764a45234
ksw <zenuine1@gmail.com>
2018-11-06 16:37:04 +0900

    production logging 설정

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,11 +1,10 @@
 from .base import *
+
 secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
 
 DEBUG = True
 
-
 WSGI_APPLICATION = 'config.wsgi.dev.application'
-
 
 DATABASES = secrets['DATABASES']
 
@@ -18,4 +17,5 @@ DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+
 
